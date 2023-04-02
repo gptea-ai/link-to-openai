e9e86b86b63f9c11c04b7a4af316f526219d0038
tsui66 <quinton.xu@gmail.com>
2023-04-03 01:35:07 +0800

    Refactor and Enhance Portal Header and Body in Render.
    - Add a deployment link to the portal header in [render.ts]
    - Rearrange and style portal header, changing its content
    - Improve the code blocks in the portal body in [render.ts]
    
    [render.ts]
    - Add a Deno deployment link to the portal header
    - Rearrange and style portal header, change its content
    - Modify the code blocks in the portal body

--- render.ts
+++ render.ts
@@ -25,7 +25,8 @@ export async function buildPortal() {
                 <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">OpenAI Proxy</h1>
                 <p class="mt-6 text-lg leading-8 text-gray-600">Unrestricted Access to OpenAI/ChatGPT - Your Key to Limitless Creativity!</p>
                 <div class="mt-10 flex items-center gap-x-6">
-                  <a href="https://github.com/gptea-ai/link-to-openai" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">View on GitHub <span aria-hidden="true">→</span></a>
+                  <a href="https://dash.deno.com/new?url=https://raw.githubusercontent.com/gptea-ai/link-to-openai/main/main.ts" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Deno Deployment</a>
+                  <a href="https://github.com/gptea-ai/link-to-openai" class="text-sm font-semibold leading-6 text-gray-900">View on GitHub <span aria-hidden="true">→</span></a>
                 </div>
               </div>
             </div>
