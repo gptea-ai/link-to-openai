aa18abdf35e22bf3d1f9500a7d2567244f312117
tsui66 <quinton.xu@gmail.com>
2023-04-03 13:25:59 +0800

    Fix missing semicolon in main.ts import statement
    - Add missing semicolon after an import statement in the main.ts file.
    
    [main.ts]
    - Add missing semicolon after import statement

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
-import { buildPortal } from "./render.ts"
+import { buildPortal } from "./render.ts";
 
 const OPEN_AI_API_HOST = "api.openai.com";
 
@@ -7,7 +7,6 @@ async function handler(request: Request): Promise<Response> {
   const url = new URL(request.url);
 
   if (url.pathname === "/") {
-    
     return new Response(await buildPortal(), {
       headers: {
         "content-type": "text/html;charset=UTF-8",
